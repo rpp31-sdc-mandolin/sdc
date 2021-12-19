ca6cbeaa2e84750c127d58183df82ff3e5167ca6
Michael <mlee85@gmail.com>
2021-12-19 01:10:02 -0800

    Aggregating all files. Need to populate new document with all products.

--- sdc_server/productAPI/product.js
+++ sdc_server/productAPI/product.js
@@ -15,9 +15,9 @@ async function main() {
 
   try {
     await client.connect();
-
+    // await createDatabase(client, 2)
     // await listDatabases(client);
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 3; i++) {
       await createDatabase(client, i);
     }
   } catch (e) {
@@ -71,18 +71,48 @@ async function createDatabase(client, number) {
       'as': 'features'
     }},
     // { '$unwind': '$features'},
+    {'$lookup': {
+      'from': 'styles',
+      'localField': 'id',
+      'foreignField': 'productId',
+      'as': 'styles'
+    }},
+    // { '$unwind': '$styles'},
     {'$addFields': {
-      'features': '$features.feature',
-      'value': '$features.value'
+      // 'features': '$features.feature',
+      // 'value': '$features.value',
+      // 'style_name': '$styles.name',
+      'style_id': '$styles.id'
+
       }
-    }
+    },
+    {'$lookup': {
+      'from': 'skus',
+      'localField': 'style_id',
+      'foreignField': 'styleId',
+      'as': 'skus'
+    }},
+    {'$lookup': {
+      'from': 'photos',
+      'localField': 'style_id',
+      'foreignField': 'styleId',
+      'as': 'photos'
+    }}
+    // {'$addFields': {
+    //   'size': '$skus.size',
+    //   'quantity': '$skus.quantity',
+    //   'url': '$photos.url',
+    //   'thumbnail_url': '$photos.thumbnail_url'
+    //   }
+    // }
   ]
 
   const test = client.db("sdc_test").collection("products_test").aggregate(pipeline)
 
 
   await test.forEach(test => {
-    console.log(`${test.id}, ${test.name}, ${test.features}, ${test.value}, ${test}`)
+    console.log(`${test.id}, ${test.name}, ${test.features}, ${test.value}, ${test.style_name}, ${test.quantity}, ${test.url}, ${test.thumbnail_url}`)
+    client.db("sdc_test").collection("document_test").insert(test);
   })
   // console.log(test);
 
