410f1b9dec3a17e74125b7cf0bdb6a28183af27e
Michael <mlee85@gmail.com>
2022-01-20 23:26:08 -0800

    Fixed Redis Typo

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 // }). catch ( err => {
 //   console.log(err)
 // })
-db.conenctToRedis();
+db.connectToRedis();
 app.get('/loaderio-7697d68ba0628325cd70a1a385052cf5', function (req, res) {
   res.send('loaderio-7697d68ba0628325cd70a1a385052cf5')
 })
